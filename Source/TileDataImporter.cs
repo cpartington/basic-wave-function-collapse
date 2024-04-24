22baa7ebe0a89af8add4cb5214639dc7abf4c696
Christie Partington <chpartin@microsoft.com>
2024-04-24 11:06:33 -0700

    Add unit test and move source into its own folder

--- Source/TileDataImporter.cs
+++ Source/TileDataImporter.cs
@@ -15,6 +15,7 @@ namespace hackathon
             using FileStream stream = File.OpenRead(TileDataFile);
             using JsonDocument jsonDocument = JsonDocument.Parse(stream);
             
+            // Populate tile data
             JsonElement data = jsonDocument.RootElement.GetProperty("tile_data");
             foreach (JsonElement tileData in data.EnumerateArray())
             {
@@ -26,6 +27,7 @@ namespace hackathon
                 // Get the tile's edge rules
                 JsonElement edgeRules = tileData.GetProperty("rules");
 
+                // Add the tile to the list
                 tiles.Add(new Tile
                 {
                     AtlasCoord = new(x, y),
@@ -34,6 +36,40 @@ namespace hackathon
                     TopEdgeType = JsonToTileType(edgeRules.GetProperty("top")),
                     BottomEdgeType = JsonToTileType(edgeRules.GetProperty("bottom"))
                 });
+            }
+
+            // Calculate tile allowances
+            for (int i = 0; i < tiles.Count; i++)
+            {
+                Tile sourceTile = tiles[i];
+                for (int j = i; j < tiles.Count; j++)
+                {
+                    Tile targetTile = tiles[j];
+                    // t | s
+                    if (sourceTile.LeftEdgeType == targetTile.RightEdgeType)
+                    {
+                        sourceTile.LeftAllowedTiles.Add(targetTile);
+                        targetTile.RightAllowedTiles.Add(sourceTile);
+                    }
+                    // s | t
+                    if (sourceTile.RightEdgeType == targetTile.LeftEdgeType)
+                    {
+                        sourceTile.RightAllowedTiles.Add(targetTile);
+                        targetTile.LeftAllowedTiles.Add(sourceTile);
+                    }
+                    // t / s
+                    if (sourceTile.TopEdgeType == targetTile.BottomEdgeType)
+                    {
+                        sourceTile.TopAllowedTiles.Add(targetTile);
+                        targetTile.BottomAllowedTiles.Add(sourceTile);
+                    }
+                    // s / t
+                    if (sourceTile.BottomEdgeType == targetTile.TopEdgeType)
+                    {
+                        sourceTile.BottomAllowedTiles.Add(targetTile);
+                        targetTile.TopAllowedTiles.Add(sourceTile);
+                    }
+                }
             }
 
             return tiles;
--- UnitTests/TileDataImporterTests.cs
+++ UnitTests/TileDataImporterTests.cs
@@ -21,5 +21,38 @@
                 Assert.IsTrue(tile.BottomEdgeType != EdgeType.Unknown, GetError(nameof(tile.BottomEdgeType)));
             }
         }
+
+        [TestMethod()]
+        public void GetTileData_CheckTileMappings()
+        {
+            List<Tile> tiles = TileDataImporter.GetTileData();
+
+            foreach (Tile tile in tiles)
+            {
+                // Every tile should have at least one tile type that can border it on each side
+                Assert.IsTrue(tile.LeftAllowedTiles.Count > 0);
+                Assert.IsTrue(tile.RightAllowedTiles.Count > 0);
+                Assert.IsTrue(tile.TopAllowedTiles.Count > 0);
+                Assert.IsTrue(tile.BottomAllowedTiles.Count > 0);
+
+                // Every tile should match up correctly
+                foreach (Tile leftTile in tile.LeftAllowedTiles)
+                {
+                    Assert.AreEqual(tile.LeftEdgeType, leftTile.RightEdgeType);
+                }
+                foreach (Tile rightTile in tile.RightAllowedTiles)
+                {
+                    Assert.AreEqual(tile.RightEdgeType, rightTile.LeftEdgeType);
+                }
+                foreach (Tile topTile in tile.TopAllowedTiles)
+                {
+                    Assert.AreEqual(tile.TopEdgeType, topTile.BottomEdgeType);
+                }
+                foreach (Tile bottomTile in tile.BottomAllowedTiles)
+                {
+                    Assert.AreEqual(tile.BottomEdgeType, bottomTile.TopEdgeType);
+                }
+            }
+        }
     }
 }
